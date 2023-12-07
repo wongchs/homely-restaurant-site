cdf560b0b964cf197551c05f22bb70a1fafade6f
Chyisiang <chyisiang8@gmail.com>
2023-12-07 18:45:08 +0800

    ported over all html pages

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -3,5 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('catalog/', views.catalog, name="catalog"),
+    path('menu/', views.menu, name="menu"),
+    path('food/', views.food, name="food"),
+    path('drinks/', views.drinks, name="drinks"),
+    path('contact/', views.contact, name="contact"),
+    path('about/', views.about, name="about"),
+    path('cart/', views.cart, name="cart")
 ]
 
--- restaurant/views.py
+++ restaurant/views.py
@@ -7,6 +7,29 @@ def home(request):
     return render(request, 'home.html')
 
 
+def menu(request):
+    return render(request, 'menu.html')
+
+
 def catalog(request):
     return render(request, 'catalog.html')
 
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def drinks(request):
+    return render(request, 'drinks.html')
+
+
+def food(request):
+    return render(request, 'food.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def cart(request):
+    return render(request, 'cart.html')
