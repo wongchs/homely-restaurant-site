b46ba715985e544730b32d5e3359ec7acda782d6
Ken Ken <95200444+KwearyKen@users.noreply.github.com>
2023-12-08 00:14:51 +0800

    Revert "awda"
    This reverts commit a106d9e78fb2e17472cb74fbe4d6f1a6f7119648.

--- restaurant/views.py
+++ restaurant/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 from .models import FoodItem
 from itertools import groupby
 from operator import attrgetter
-from django.http import JsonResponse
 
 
 # Create your views here.
@@ -48,32 +47,3 @@ def cart(request):
 def item_detail(request, pk):
     item = FoodItem.objects.get(id = pk)
     return render(request, 'item_detail.html', {'item': item})
-
-cart = []
-
-def get_cart(request):
-    # Your logic to fetch cart data goes here
-    # For example, let's assume you have a list of items in the cart
-    cart_data = [
-        {'name': 'Product 1', 'price': 10.0, 'quantity': 2},
-        {'name': 'Product 2', 'price': 5.0, 'quantity': 1},
-        # Add more items as needed
-    ]
-
-    # Return a JSON response with cart data
-    return JsonResponse(cart_data, safe=False)
-
-
-def add_to_cart(request):
-    # Dummy data for a new item to add to the cart
-    new_item = {
-        'name': request.GET.get('name', ''),
-        'price': float(request.GET.get('price', 0)),
-        'quantity': int(request.GET.get('quantity', 1)),
-    }
-
-    # Add the new item to the cart
-    cart.append(new_item)
-
-    # Return a JSON response indicating success
-    return JsonResponse({'success': True})
