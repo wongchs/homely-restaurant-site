52e4345402e5e5759c1bfc7a712fcf456a62cb03
Ken Ken <95200444+KwearyKen@users.noreply.github.com>
2023-12-08 01:27:40 +0800

    individual items

--- restaurant/models.py
+++ restaurant/models.py
@@ -13,9 +13,12 @@ class FoodItem(models.Model):
         return f'{self.type} - {self.name}'
 
 class Product(models.Model):
+    type = models.CharField(max_length=100)
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField()
     image = models.ImageField(upload_to='product_images/')
+    details_url = models.URLField(blank=True, null=True)
 
     def __str__(self):
-        return self.name
+        return f'{self.type} - {self.name}'
