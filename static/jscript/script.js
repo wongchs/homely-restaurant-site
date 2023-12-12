94ac2b8f01d1ff45175b6eb4d3287a5656d8aeb2
Chyisiang <chyisiang8@gmail.com>
2023-12-12 19:03:35 +0800

    cart styling

--- static/jscript/script.js
+++ static/jscript/script.js
@@ -1,6 +1,6 @@
 function changeQuantity(button, delta) {
-  var quantityInput = button.parentElement.querySelector(".quantity-input");
-  var currentQuantity = parseInt(quantityInput.value);
+  let quantityInput = button.parentElement.querySelector(".quantity-input");
+  let currentQuantity = parseInt(quantityInput.value);
   if (currentQuantity + delta > 0) {
     quantityInput.value = currentQuantity + delta;
   }
